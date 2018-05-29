da64e449750587b73cbe7eadd384b44bbd91007c
ogonchar <37533220+ogonchar@users.noreply.github.com>
2018-05-29 09:08:24 +0300

    asc

--- TenderStore/src/main/java/com/packt/webstore/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.packt.webstore.domain.repository;
-
-import com.packt.webstore.domain.User;
-
-public interface UserRepository {
-	
-	public void addUser(User user);
-
-}
--- TenderStore/src/main/java/com/packt/webstore/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.packt.webstore.service;
-
-import com.packt.webstore.domain.User;
-
-public interface UserService {
-	
-	public void addUser(User user);
-	
-}
